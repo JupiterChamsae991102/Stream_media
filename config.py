8a6746f25b2297297eedd6e46107e486e33dfa84
JupiterChamsae991102 <42896902+JupiterChamsae991102@users.noreply.github.com>
2018-09-22 15:03:00 -0500

    Configuration
    API Configuration

---
+++ config.py
@@ -0,0 +1,6 @@
+# following credentials should be obtained from by registering on twitter api
+consumer_key = 'consumer_key' 
+consumer_secret = 'consumer_secret'
+access_token = 'access_token'
+access_secret = 'access_secret'
+
